704be350825475465c5d2e70c4c274995e228e15
Elina <ehaverin@dhcp-asv-82.helsinki.fi>
2015-01-21 00:02:16 +0200

    hahmoteltu Kysymys-luokkaa

--- visailu/src/main/java/sovellus/domain/Kysymys.java
+++ visailu/src/main/java/sovellus/domain/Kysymys.java
@@ -1,6 +1,38 @@
-
 package sovellus.domain;
 
+import java.util.ArrayList;
+
 public class Kysymys {
+
+    //tähän tarvitsisi ehkäpä muuttujiksi:
+    //kysymyssanan
+    //oikean vastauksen
+    //ArrayListin, jossa kysymykseen liittyvät väärät vastausvaihtoehdot
     
+    public Kysymys() {
+        //alustetaan arraylist
+    }
+
+    public void lisaaKysymyssana(String kysymyssana) {
+    }
+
+    public void lisaaOikeaVastaus(String oikeaVastaus) {
+    }
+
+    public void lisaaVaaratVastaukset() {
+        //metodin parametrina väärät vastaukset
+        //lisätään for:illa arraylistiinn
+    }
+
+    public String getKysymyssana() {
+        return null;
+    }
+
+    public String getOikeaVastaus() {
+        return null;
+    }
+
+    public ArrayList<String> getVaaratVastaukset() {
+        return null;
+    }
 }
