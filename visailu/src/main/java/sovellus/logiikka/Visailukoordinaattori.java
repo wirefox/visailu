384d12b30503fb7f1ba227a070e3e83331f3c5d7
Elina <ehaverin@dhcp-asv-82.helsinki.fi>
2015-01-20 22:24:14 +0200

    hahmoteltu visailukoordinaattori-luokan toimintaa

--- visailu/src/main/java/sovellus/logiikka/Visailukoordinaattori.java
+++ visailu/src/main/java/sovellus/logiikka/Visailukoordinaattori.java
@@ -1,9 +1,24 @@
-
 package sovellus.logiikka;
 
 public class Visailukoordinaattori {
 
     public void kaynnista() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Hei, tervetuloa visailuun!");
+        System.out.println("");
+
+        //luo uusi tiedostonlukija, ja luetuta tiedosto HashMapiin (maa, pääkaupunki)
+        //luo uusi tiedonkäsittelijä, joka muodostaa luetusta tiedostosta
+        //kysymyksiä sisältävän kysymyssarjan (maa + oikea vastaus sekä
+        //neljä muuta vastausvaihtoehtoa
+        
+        //visaile() (oma metodinsa)
+        // pelinLopetus() (oma metodinsa)
     }
+    
+    //visaile-metodi, jossa ohjataan pelin kulkua, eli luodaan uusi peli,
+    //ja pyydetään peliltä uutta kierrosta niin kauan kunnes on
+    // pelin viimeinen kierros
+    
+    //pelinLopetus-metodi, jossa vikan kierroksen jälkeen annetaan pelaajalle
+    //arvio hänen suoriutumisestaan
 }
