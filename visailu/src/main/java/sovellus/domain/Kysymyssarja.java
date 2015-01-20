c0b3b24a86070795d53aacbf5667e83bb57ac489
Elina <ehaverin@dhcp-asv-82.helsinki.fi>
2015-01-21 00:05:03 +0200

    hahmoteltu Kysymyssarja-luokkaa

--- visailu/src/main/java/sovellus/domain/Kysymyssarja.java
+++ visailu/src/main/java/sovellus/domain/Kysymyssarja.java
@@ -1,6 +1,18 @@
-
 package sovellus.domain;
 
 public class Kysymyssarja {
-    
+    // tähän tarvinnee muuttujaksi:
+    //ArrayListin, jossa on kysymykset
+
+    public Kysymyssarja() {
+        //ArrayListin alustus
+    }
+
+    public void lisaaKysymys(Kysymys kysymys) {
+        //kysymyksen lisäys ArrayListiin
+    }
+
+    public Kysymys getKysymys(int i) {
+        return null;
+    }
 }
