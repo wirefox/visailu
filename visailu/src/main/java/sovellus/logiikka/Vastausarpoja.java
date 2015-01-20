2f6c5a1cd0bb3507b6da05161e9730e8c53b38b5
Elina <ehaverin@dhcp-asv-82.helsinki.fi>
2015-01-20 23:34:53 +0200

    ohjelmoitu vastausarpojan, eli väärien vastausvaihtoehtojen toiminnallisuus alustavasti

--- visailu/src/main/java/sovellus/logiikka/Vastausarpoja.java
+++ visailu/src/main/java/sovellus/logiikka/Vastausarpoja.java
@@ -1,6 +1,19 @@
-
 package sovellus.logiikka;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Vastausarpoja {
-    
+
+    public ArrayList<String> arvoVastauksetKysymykselle(ArrayList<String> listaVastauksia) {
+        ArrayList<String> vastaukset = new ArrayList<String>();
+
+        //arvotaan yhdelle kysymykselle neljä väärää vastausvaihtoehtoa
+        for (int i = 0; i < 4; i++) {
+            Random arpoja = new Random();
+            int vastauksenNro = arpoja.nextInt(listaVastauksia.size());
+            vastaukset.add(listaVastauksia.get(vastauksenNro));
+        }
+        return vastaukset;
+    }
 }
