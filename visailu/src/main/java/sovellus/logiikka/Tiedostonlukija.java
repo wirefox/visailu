1ddb87b33f2ff6369f1f37c2ab97b8c5190f1bbf
Elina <ehaverin@dhcp-asv-82.helsinki.fi>
2015-01-20 22:55:15 +0200

    tehty Tiedostonlukija-luokka, jossa tiedosto luetaan sisään ja pilkotaan HashMapiin talletettavaksi

--- visailu/src/main/java/sovellus/logiikka/Tiedostonlukija.java
+++ visailu/src/main/java/sovellus/logiikka/Tiedostonlukija.java
@@ -1,6 +1,41 @@
-
 package sovellus.logiikka;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Tiedostonlukija {
-    
+
+    private Scanner lukija;
+    private HashMap<String, String> kysymysJaVastaus;
+
+    public Tiedostonlukija() {
+        this.kysymysJaVastaus = new HashMap<String, String>();
+    }
+
+    public HashMap<String, String> lueTiedosto() {
+        File tiedosto = new File("maatJaPaakaupungit.txt");
+
+        try {
+            this.lukija = new Scanner(tiedosto);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Tiedostonlukija.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        ositaTiedostostaLuettuTekstiJaTalleta();
+        return this.kysymysJaVastaus;
+    }
+
+    private void ositaTiedostostaLuettuTekstiJaTalleta() {
+        while (lukija.hasNextLine()) {
+            String rivi = lukija.nextLine();
+            String[] osat = rivi.split(",");
+            String osa1 = osat[0];
+            String osa2 = osat[1];
+            this.kysymysJaVastaus.put(osa1, osa2);
+        }
+    }
 }
