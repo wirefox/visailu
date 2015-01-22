faffb685f422ade02b002f50465ffcbf286fd745
Elina <ehaverin@dhcp-asv-82.helsinki.fi>
2015-01-22 20:50:38 +0200

    muokattu vastausarpojaa siten, että se ei voi arpoa väärien vastausten joukkoon samoja vaihtoehtoja

--- visailu/src/main/java/sovellus/logiikka/Vastausarpoja.java
+++ visailu/src/main/java/sovellus/logiikka/Vastausarpoja.java
@@ -9,10 +9,14 @@ public class Vastausarpoja {
         ArrayList<String> vastaukset = new ArrayList<String>();
 
         //arvotaan yhdelle kysymykselle neljä väärää vastausvaihtoehtoa
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; vastaukset.size() < 4; i++) {
             Random arpoja = new Random();
             int vastauksenNro = arpoja.nextInt(listaVastauksia.size());
-            vastaukset.add(listaVastauksia.get(vastauksenNro));
+            String vastaus = listaVastauksia.get(vastauksenNro);
+
+            if (!vastaukset.contains(vastaus)) {
+                vastaukset.add(vastaus);
+            }
         }
         return vastaukset;
     }
