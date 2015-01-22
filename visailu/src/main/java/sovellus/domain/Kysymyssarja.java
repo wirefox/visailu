d2bc8f5c66c509fd90e39aba6bbfe8e4fef1b676
Elina <ehaverin@dhcp-asv-82.helsinki.fi>
2015-01-22 20:41:26 +0200

    muokattu kysymyssarja-luokan toiminnallisuutta, nyt toimii

--- visailu/src/main/java/sovellus/domain/Kysymyssarja.java
+++ visailu/src/main/java/sovellus/domain/Kysymyssarja.java
@@ -8,11 +8,9 @@ public class Kysymyssarja {
 
     public Kysymyssarja() {
         this.kysymykset = new ArrayList<Kysymys>();
-        System.out.println("TESTI1");
     }
 
     public void lisaaKysymys(Kysymys kysymys) {
-        System.out.println("TESTI2");
         this.kysymykset.add(kysymys);
     }
 
