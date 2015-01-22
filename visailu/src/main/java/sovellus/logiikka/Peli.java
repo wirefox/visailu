fdb083054e8eaa23c6cc0df5ddf828a275d56661
Elina <ehaverin@dhcp-asv-82.helsinki.fi>
2015-01-22 19:17:22 +0200

    tehty peli-luokan toiminnallisuutta. visailu ei vielä toimi, koska kysymys- ja kysymyssarja -luokkien toiminnallisuus puutteellinen

--- visailu/src/main/java/sovellus/logiikka/Peli.java
+++ visailu/src/main/java/sovellus/logiikka/Peli.java
@@ -1,56 +1,82 @@
 package sovellus.logiikka;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+import sovellus.domain.Kysymys;
 import sovellus.domain.Kysymyssarja;
 
 public class Peli {
-
+    
     private Kysymyssarja kysymyssarja;
-    //tässä pitäisi ehkä olla muuttujina:
-    //kierroksen numero
-    //kuinka paljon pelaajalla tällä hetkellä pisteitä
-    //Kysymyssarja
-    //Scanner, nyt kun ei ole graafista käyttistä
-    //oikea vastaus
-
+    private Kysymys kysymys;
+    private int pisteitaPelaajalla;
+    private int kierroksenNumero;
+    private Scanner lukija;
+    
     public Peli(Kysymyssarja kysymyssarja) {
         this.kysymyssarja = new Kysymyssarja();
+        this.kysymys = new Kysymys();
+        this.pisteitaPelaajalla = 0;
+        this.kierroksenNumero = 1;
     }
-
+    
     public void pelaaKierros() {
-        System.out.println("Mikä on ylläolevan valtion pääkaupunki, valitse allaolevista vaihtoehdoista:");
-        annaVaihtoehdot();
-
+        System.out.println("Mikä on ylläolevan valtion pääkaupunki, valitse vaihtoehdoista:");
+        annaKysymyssana();
+        annaVastausvaihtoehdot();
+        
         System.out.print("Kirjoita arvauksesi: ");
-        //luetaan pelaajan vastaus
-
-        // annetaan pelaajan vastaus VastauksenArviointi(vastaus)-metodille.
+        String vastaus = this.lukija.nextLine();
+        
+        vastauksenArviointi(vastaus);
+        
         pistetilanteenTulostus();
-
-        //lisätään kierroksen numeroa
+        this.kierroksenNumero++;
     }
-
+    
     public boolean onkoVikaKierros() {
-        // tässä metodissa tarkastetaan, onko kyseessä viimeinen kierros.
-        // eli jatketaanko pelaamista vai ei
-        return true;
+        if (this.kierroksenNumero > 20) {
+            return true;
+        } else {
+            return false;
+        }
     }
-
-    public int getPisteitaKasassa() {
-        return -99;
+    
+    public int getPisteitaPelaajalla() {
+        return this.pisteitaPelaajalla;
     }
-
-    private void annaVaihtoehdot() {
-        // tässä metodissa annetaan pelaajalle kaikki vastausvaihtoehdot
+    
+    private void annaKysymyssana() {
+        this.kysymys = this.kysymyssarja.getKysymys(this.kierroksenNumero - 1);
+        System.out.println(this.kysymyssarja.getKysymys(kierroksenNumero));
+        System.out.println(this.kysymys.getKysymyssana().toUpperCase());
+        System.out.println("");
     }
-
+    
+    private void annaVastausvaihtoehdot() {
+        ArrayList<String> vastausvaihtoehdot = this.kysymys.getVaaratVastaukset();
+        vastausvaihtoehdot.add(this.kysymys.getOikeaVastaus());
+        Collections.shuffle(vastausvaihtoehdot);
+        for (String paakaupunki : vastausvaihtoehdot) {
+            System.out.println(paakaupunki);
+        }
+        System.out.println("");
+    }
+    
     private void vastauksenArviointi(String vastaus) {
-        //tässä metodissa tarkastetaan pelaajan vastaus:
-        //jos oikea vastaus, tulee yksi piste lisää
-        //jos väärä vastaus, kerrotaan mikä olisi ollut oikea vastaus
+        //tässä metodissa tarkastetaan pelaajan vastaus
+        if (vastaus.toUpperCase().equals(this.kysymys.getOikeaVastaus().toUpperCase())) {
+            System.out.println("Hienoa, oikea vastaus!");
+            this.pisteitaPelaajalla++;
+        } else {
+            System.out.println("Nyt meni väärin");
+            System.out.println("Oikea vastaus olisi ollut " + this.kysymys.getOikeaVastaus());
+        }
     }
-
+    
     private void pistetilanteenTulostus() {
-        //Tässä metodissa kerrotaan pelaajalle kuinka paljon hänellä on pisteitä
-        //muodossa pelaajan pisteet / kaikki kysytyt kysymykset
+        System.out.println(this.pisteitaPelaajalla + " / " + (this.kierroksenNumero));
+        System.out.println("");
     }
 }
