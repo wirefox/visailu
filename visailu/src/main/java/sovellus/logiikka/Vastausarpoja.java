c81b3493d33fe2f21ebab4c8db8da90de3e99a16
Elina <ehaverin@dhcp-asv-82.helsinki.fi>
2015-01-26 16:51:29 +0200

    korjattu vastausarpoja-luokkaa

--- visailu/src/main/java/sovellus/logiikka/Vastausarpoja.java
+++ visailu/src/main/java/sovellus/logiikka/Vastausarpoja.java
@@ -2,20 +2,25 @@ package sovellus.logiikka;
 
 import java.util.ArrayList;
 import java.util.Random;
+import sovellus.domain.Kysymys;
 
 public class Vastausarpoja {
+
+    private Kysymys kysymys;
+
+    public Vastausarpoja(Kysymys kysymys) {
+        this.kysymys = kysymys;
+    }
 
     public ArrayList<String> arvoVastauksetKysymykselle(ArrayList<String> listaVastauksia) {
         ArrayList<String> vastaukset = new ArrayList<String>();
 
-        //TODO: vastausarpojan pitää tietää ko. kysymys ja oikea vastaus,
-        //jottei se arvo oikeaa vastausta vääräksi
         for (int i = 0; vastaukset.size() < 4; i++) {
             Random arpoja = new Random();
             int vastauksenNro = arpoja.nextInt(listaVastauksia.size());
             String vastaus = listaVastauksia.get(vastauksenNro);
 
-            if (!vastaukset.contains(vastaus)) {
+            if (!vastaukset.contains(vastaus) && !this.kysymys.getOikeaVastaus().equals(vastaus)) {
                 vastaukset.add(vastaus);
             }
         }
