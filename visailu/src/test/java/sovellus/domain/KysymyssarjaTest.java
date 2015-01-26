0b6673d66d8b4ad1ed2ce38d5a4c1e16b9f5557d
Elina <ehaverin@dhcp-asv-82.helsinki.fi>
2015-01-26 19:57:16 +0200

    muokattu kysymyssarja-testiluokkaa

--- visailu/src/test/java/sovellus/domain/KysymyssarjaTest.java
+++ visailu/src/test/java/sovellus/domain/KysymyssarjaTest.java
@@ -1,7 +1,6 @@
 package sovellus.domain;
 
-
-
+import java.util.ArrayList;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -10,26 +9,49 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class KysymyssarjaTest {
-    
+
+    private ArrayList<Kysymys> kysymykset;
+
     public KysymyssarjaTest() {
+        this.kysymykset = new ArrayList<Kysymys>();
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
+        Kysymys kysymys = new Kysymys();
+        kysymys.lisaaKysymyssana("Espanja");
+        kysymys.lisaaOikeaVastaus("Madrid");
+
+        ArrayList<String> vaaratVastaukset = new ArrayList();
+        vaaratVastaukset.add("Helsinki");
+        vaaratVastaukset.add("Tukholma");
+        vaaratVastaukset.add("Berliini");
+        vaaratVastaukset.add("Moskova");
+
+        kysymys.lisaaVaaratVastaukset(vaaratVastaukset);
+
+        this.kysymykset.add(kysymys);
     }
-    
+
     @After
     public void tearDown() {
     }
 
-     @Test
-     public void hello() {}
+    @Test
+    public void kysymyksenLisaamisenJalkeenListanKokoOnYksi() {
+        assertEquals(1, this.kysymykset.size());
+    }
+    
+    @Test
+    public void kysymyssarjastaEiArvotaListanEnsimmäistä() {
+        
+    }
 }
