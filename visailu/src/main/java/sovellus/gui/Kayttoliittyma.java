e310f69df69d15c307133e63c787a0f0f3c75394
Elina <ehaverin@dhcp-asv-82.helsinki.fi>
2015-01-26 22:49:22 +0200

    aloitettu hahmottelemaan käyttöliittymäluokkia

--- visailu/src/main/java/sovellus/gui/Kayttoliittyma.java
+++ visailu/src/main/java/sovellus/gui/Kayttoliittyma.java
@@ -1,6 +1,91 @@
-
 package sovellus.gui;
 
-public class Kayttoliittyma {
-    
+import java.awt.Container;
+import java.awt.Dimension;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.WindowConstants;
+
+public class Kayttoliittyma implements Runnable {
+
+    private JFrame frame;
+
+    public Kayttoliittyma() {
+    }
+
+    @Override
+    public void run() {
+        frame = new JFrame("Visailun käynnistysikkuna");
+        frame.setPreferredSize(new Dimension(200, 100));
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        luoKomponentit(frame.getContentPane());
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void luoKomponentit(Container container) {
+        JLabel teksti = new JLabel("Moikka! Tervetuloa visailuun!");
+        container.add(teksti);
+
+        JButton nappi = new JButton("Aloita peli");
+        nappi.addActionListener(new Tapahtumankuuntelija());
+
+        container.add(nappi);
+    }
+
+    public JFrame getFrame() {
+        return frame;
+    }
 }
+
+//Miten itse pelin ikkuna kannattaa toteuttaa? Oma ikkunansa, vai samaan, 
+//missä peli käynnistetään?
+//@Override
+//    public void run() {
+//        frame = new JFrame("Visailu");
+//        frame.setPreferredSize(new Dimension(200, 100));
+//
+//        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//
+//        luoKomponentit(frame.getContentPane());
+//
+//        frame.pack();
+//        frame.setVisible(true);
+//    }
+//
+//    private void luoKomponentit(Container container) {
+//        BoxLayout layout = new BoxLayout(container, BoxLayout.Y_AXIS);
+//        container.setLayout(layout);
+//
+//        container.add(new JLabel("Valitse kaupunki, joka on valtion pääkaupunki"));
+//
+//        JRadioButton vaihtoehto1 = new JRadioButton("Vaihtoehto 1");
+//        JRadioButton vaihtoehto2 = new JRadioButton("Vaihtoehto 2");
+//        JRadioButton vaihtoehto3 = new JRadioButton("Vaihtoehto 3");
+//        JRadioButton vaihtoehto4 = new JRadioButton("Vaihtoehto 4");
+//        JRadioButton vaihtoehto5 = new JRadioButton("Vaihtoehto 5");
+//
+//        ButtonGroup buttonGroup = new ButtonGroup();
+//        buttonGroup.add(vaihtoehto1);
+//        buttonGroup.add(vaihtoehto2);
+//        buttonGroup.add(vaihtoehto3);
+//        buttonGroup.add(vaihtoehto4);
+//        buttonGroup.add(vaihtoehto5);
+//
+//        container.add(vaihtoehto1);
+//        container.add(vaihtoehto2);
+//        container.add(vaihtoehto3);
+//        container.add(vaihtoehto4);
+//        container.add(vaihtoehto5);
+//    }
+//
+//    public JFrame getFrame() {
+//        return frame;
+//    }
--- visailu/src/main/java/sovellus/gui/Tapahtumankuuntelija.java
+++ visailu/src/main/java/sovellus/gui/Tapahtumankuuntelija.java
@@ -1,6 +1,12 @@
-
 package sovellus.gui;
 
-public class Tapahtumankuuntelija {
-    
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Tapahtumankuuntelija implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        System.out.println("Aloita-nappia klikattu!");
+    }
 }
