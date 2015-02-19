6a21d3a5571b2f56a3106844bca4087449762aaf
Elina <ehaverin@dhcp-asv-82.helsinki.fi>
2015-02-19 21:52:04 +0200

    muokattu visailukoordinaattoritestitietoa; mitenhän tätä luokkaa ja sen metodeja testaisi...

--- visailu/src/test/java/sovellus/logiikka/VisailukoordinaattoriTest.java
+++ visailu/src/test/java/sovellus/logiikka/VisailukoordinaattoriTest.java
@@ -7,14 +7,19 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import sovellus.domain.Kysymys;
 import sovellus.domain.Kysymyssarja;
+import sovellus.gui.GraafinenKayttoliittyma;
 import sovellus.gui.Tekstikayttoliittyma;
 
 public class VisailukoordinaattoriTest {
 
     private String kysymyslause;
+    private Kysymys kysymys;
     private Tiedostonlukija tiedostonlukija;
-    private Peli peli;
+    private Tiedonkasittelija tiedonkasittelija;
+    private Kysymyssarja kysymyssarja;
+    //   private Peli peli;
 
     public VisailukoordinaattoriTest() {
 
@@ -37,12 +42,31 @@ public class VisailukoordinaattoriTest {
     public void tearDown() {
     }
 
-    //TÄMÄ ON IHAN HÖPÖLÖPÖ-TESTI
+//    //TÄMÄ ON IHAN HÖPÖLÖPÖ-TESTI
+//    @Test
+//    public void kaynnistysmetodinJalkeenOnMuodostettuKysymyssarja() {
+//        Visailukoordinaattori visailukoordinaattori = new Visailukoordinaattori();
+//        visailukoordinaattori.kaynnistaGUI();
+//
+//        assertEquals("Mikä on allaolevan valtion pääkaupunki, valitse vaihtoehdoista: ", this.kysymyslause);
+//    }
     @Test
-    public void kaynnistysmetodinJalkeenOnMuodostettuKysymyssarja() {
+    public void pelinValmistelutoimienAikanaMuodostetaanKysymyssarjaJossaKysymyksia() {
+        //GraafinenKayttoliittyma kayttis = new GraafinenKayttoliittyma(this.peli);
         Visailukoordinaattori visailukoordinaattori = new Visailukoordinaattori();
-        visailukoordinaattori.kaynnistaGUI();
+        this.tiedostonlukija = new Tiedostonlukija();
+        String kysymyslause = this.tiedostonlukija.getKysymyslause();
+        HashMap<String, String> kysymyksetJaVastaukset = new HashMap<String, String>();
+        this.tiedonkasittelija = new Tiedonkasittelija(kysymyksetJaVastaukset, kysymyslause);
+        this.kysymyssarja = this.tiedonkasittelija.muodostaKysymyssarja();
+        Peli peli = new Peli(kysymyslause, this.kysymyssarja);
+        this.kysymys = peli.getKysymys();
+        assertNotNull(this.kysymys);
+    }
 
-        assertEquals("Mikä on allaolevan valtion pääkaupunki, valitse vaihtoehdoista: ", this.kysymyslause);
+    @Test
+    public void visailukoordinaattoriLuodaan() {
+        Visailukoordinaattori visailukoordinaattori = new Visailukoordinaattori();
+        assertNotNull(visailukoordinaattori);
     }
 }
